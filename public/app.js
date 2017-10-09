06247349910d4f5eb689f1265c2e87e14672a3aa
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-10-08 23:42:34 -0500

    add mute/unmute

--- public/app.js
+++ public/app.js
@@ -3,6 +3,17 @@ var radioApp = angular.module('radioApp', []);
 radioApp.controller('RadioController', function RadioController($scope, $http) {
   $scope.song = 'test';
 
+  var sound = new Howl({
+    src: ['http://96.31.83.86:8091/;'],
+    format: ['mpeg'],
+    autoplay: true,
+    html5: true,
+    mobileAutoEnable: true,
+    loop: true,
+    volume: 0.8,
+    mute: false
+  });
+
   $scope.init = function() {
     function getRequest() {
       $http.get('/currentSong').then(
@@ -18,5 +29,20 @@ radioApp.controller('RadioController', function RadioController($scope, $http) {
     setInterval(function() {
       getRequest();
     }, 20000);
+  };
+
+  $scope.play = function() {
+    if (sound._muted === true) {
+      console.log('unmuted');
+      sound.mute(false);
+    } else {
+      console.log('press play');
+      sound.play();
+    }
+  };
+
+  $scope.pause = function() {
+    sound.mute(true);
+    console.log(sound);
   };
 });
